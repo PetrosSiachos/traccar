bb067c6c12827ef4c8431825af23521b7a26e414
PetrosSiachos <t8160203@aueb.gr>
2020-06-09 17:59:05 +0300

    import appropriate package

--- src/test/java/org/traccar/BaseProtocolTest.java
+++ src/test/java/org/traccar/BaseProtocolTest.java
@@ -3,7 +3,7 @@ package org.traccar;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import org.traccar.BaseProtocol;
 public class BaseProtocolTest {
 	BaseProtocol mm = new BaseProtocol();
 	
