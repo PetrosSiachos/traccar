02ee025ae7b0ea1c438b437196c092ef8aeb3510
PetrosSiachos <t8160203@aueb.gr>
2020-06-09 18:08:35 +0300

    Change constructor

--- src/test/java/org/traccar/BaseProtocolTest.java
+++ src/test/java/org/traccar/BaseProtocolTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.traccar.BaseProtocol;
 public class BaseProtocolTest {
-	BaseProtocol mm = new BaseProtocol();
+	BaseProtocol mm = new Protocol();
 	
 	@Test (expected = RuntimeException.class)
 	public void testSendTextCommand() {
